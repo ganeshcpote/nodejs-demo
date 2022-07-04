a56c909aa78970adf64645900c58074180faf3a5
Adhi-AWS <90025213+Adhi-AWS@users.noreply.github.com>
2022-07-04 15:55:36 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.232.132.138:9000/',
+  serverUrl: 'http://65.0.42.18:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '8cc5903f1a3b7ec268634aaf609048c2214e8e42',
+	    'sonar.projectName': 'Jenkins-NodeJS',
+	    'sonar.projectKey':'Jenkins-NodeJS',
+	    'sonar.login': 'sqp_689e416a26131937a7623ca25e1e76171a9a413a',
 	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    //'sonar.password': 'Admin@123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
